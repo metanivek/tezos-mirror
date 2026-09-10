a57ebf51a57549e7784383ff17eca70158ba88a6
Thomas Letan <lthms@nomadic-labs.com>
2026-09-10 16:23:24 +0200

    EVM Node: Log’s `position` field is optional
    No released kernel support it.

--- etherlink/bin_node/lib_dev/encodings/tracer_types.mli
+++ etherlink/bin_node/lib_dev/encodings/tracer_types.mli
@@ -124,7 +124,7 @@ module CallTracer : sig
     address : Ethereum_types.address;
     topics : Ethereum_types.hex list;
     data : Ethereum_types.hex;
-    position : Ethereum_types.quantity;
+    position : Ethereum_types.quantity option;
   }
 
   type output = {
